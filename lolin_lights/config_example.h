b87359141303e7224f3ddf1efac393b649a68855
Michael Erskine <michael.erskine@geoslam.com>
2017-12-20 11:50:26 +0000

    provide example config

---
+++ lolin_lights/config_example.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define SET_API_SERVER    "192.168.0.99"
+#define SET_API_PORT      "9999"
+
+#define URL_OFF           "http://" SET_API_SERVER ":" SET_API_PORT "/off/x/y"
+#define URL_ON            "http://" SET_API_SERVER ":" SET_API_PORT "/on/x/y"
+
+#define AP_SSID           "whateverap"
+#define AP_PASSPHRASE     "whateverpass"
