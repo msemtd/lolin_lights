cbf0062e51df7cbf069e7d36f16146cd2293a6a2
Michael Erskine <michael.erskine@geoslam.com>
2023-11-15 13:16:23 +0000

    update before machine wipe!

--- lolin_lights/lolin_lights.ino
+++ lolin_lights/lolin_lights.ino
@@ -2,6 +2,8 @@
  *  LoLin nodemcu web API lightswitch
  *
  */
+#define MSEPRODUCT "LoLin nodemcu web API lightswitch"
+#define MSEVERSION "v1.0"
 
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
@@ -11,7 +13,20 @@
 ESP8266WiFiMulti WiFiMulti;
 
 #include "config.h"
+#ifndef AP_SSID
+#error "You need to define some variables in config.h or perhaps here..."
+#define AP_SSID         "your_access_point"
+#define AP_PASSPHRASE   "your_ap_password"
+// You can also set further AP definitions here - see setup function
+#define SET_API_SERVER  "your_server_address"
+#define SET_API_PORT    "80"
+#define URL_OFF         "http://" SET_API_SERVER ":" SET_API_PORT "/off_path"
+#define URL_ON          "http://" SET_API_SERVER ":" SET_API_PORT "/on_path"
+#endif
 
+wl_status_t wfs = WL_DISCONNECTED;
+
+// Digital I/O --------------------------------------------------------------
 // map lolin DIO pins to arduino IDE pin numbers...
 // D0, D1, D2, D3, D4, D5, D6, D7, D8
 // 16,  5,  4,  0,  2, 14, 12, 13, 15
@@ -20,40 +35,66 @@ ESP8266WiFiMulti WiFiMulti;
 const uint8_t d_map[8] = {16, 5, 4, 0, 2, 14, 12, 13};
 static uint8_t d_inputs = 0;
 static debounce8_t d_deb;
+
+// Application control ------------------------------------------------------
 bool light_on = false;
+// Current time read once per loop...
+uint32_t now = 0;
+
+// Diagnostic console -------------------------------------------------------
+// When using a console line reading mode, append non-line-ending char to input
+// string (if not already overflowed).
+// Both CR and LF can terminate the line and ESC (0x1b) can clear the line.
+#define DIAG_INPUT_MAX 128
+String consoleInput = "";
+bool consoleLineMode = false;
+bool consoleDebug = false;
+
+void version() {
+    Serial.println(MSEPRODUCT " " MSEVERSION);
+}
 
 void setup() {
     inputs_setup();
+    consoleInput.reserve(DIAG_INPUT_MAX);
     Serial.begin(115200);
     delay(10);
-    // Connect to a WiFi network...
+    Serial.println();
+    Serial.println();
+    version();
     WiFiMulti.addAP(AP_SSID, AP_PASSPHRASE);
-    Serial.println();
-    Serial.println();
-    Serial.print("Wait for WiFi... ");
-    while(WiFiMulti.run() != WL_CONNECTED) {
-        Serial.print(".");
-        delay(500);
-    }
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    delay(500);
+#ifdef AP_SSID2
+    WiFiMulti.addAP(AP_SSID2, AP_PASSPHRASE2);
+#endif
+#ifdef AP_SSID3
+    WiFiMulti.addAP(AP_SSID3, AP_PASSPHRASE3);
+#endif
+    Serial.print("Waiting for WiFi... ");
+    wfs = WiFiMulti.run();
 }
 
 void loop() {
-  bloop(light_on);
-  light_on = !light_on;
-  Serial.println("wait 5 sec...");
-  delay(5000);
+    now = millis();
+    task_inputs();
+    // acting on buttons - pressed? held? how long? auto-repeat?
+
+    // bloop(light_on);
+    // light_on = !light_on;
+    // Serial.println("wait 5 sec...");
+    task_serial_read();
 }
 
 void bloop(bool tog) {
     const char * host = SET_API_SERVER; // ip or dns
     const char * url_on = URL_ON;
     const char * url_off = URL_OFF;
-    
+
+    wfs = WiFiMulti.run();
+    if(wfs != WL_CONNECTED) {
+        Serial.println("No AP connection");
+        return;
+    }
+
     Serial.print("connecting to ");
     Serial.println(host);
     HTTPClient http;
@@ -85,7 +126,7 @@ void inputs_setup(void) {
     d_inputs = inputs_read();
 }
 
-uint16_t inputs_read(void) {
+uint8_t inputs_read(void) {
     uint8_t d_samp = 0;
     for (int i = 0; i < 8; i++) {
         bitWrite(d_samp, i, (digitalRead(d_map[i]) ? 0 : 1));
@@ -99,7 +140,8 @@ uint16_t inputs_read(void) {
 void task_inputs(void)
 {
   static uint32_t last;
-  uint32_t now = millis();
+  // We have a global "now" updated every loop...
+  // uint32_t now = millis();
   if (now - last > 5) {
     last = now;
     uint8_t d_samp = inputs_read();
@@ -107,8 +149,119 @@ void task_inputs(void)
     if (d_samp != d_inputs) {
       d_inputs = d_samp;
       Serial.print("Inputs: ");
-      Serial.println(d_inputs, BIN);
+      Serial.println(d_inputs, HEX);
     }
   }
 }
 
+void task_serial_read(void) {
+      while(Serial.available()){
+        proc_console_input(Serial.read());
+    }
+}
+
+int proc_console_input(int k) {
+    // When using a console line reading mode, append non-line-ending char to input 
+    // string (if not already overflowed).
+    // Both CR and LF can terminate the line and ESC (0x1b) can clear the line.
+    if(consoleLineMode) {
+        switch(k) {
+        case 10:
+        case 13:
+            proc_console_line();
+            consoleInput = "";
+            return 0;
+        case 27:
+            consoleInput = "";
+            if(consoleDebug)
+                Serial.println("ESC");
+            return 0;
+        default:
+            // TODO backspace and arrow chars, useful stuff
+            if(consoleInput.length() < DIAG_INPUT_MAX) {
+                // ascii printable 0x20 to 0x7E
+                if(k < 0x20 || k > 0x7E) {
+                    // non-printable - do nothing
+                    if(consoleDebug) {
+                        Serial.print("non-ascii printable: ");
+                        Serial.println(k, DEC);
+                    }
+                } else {
+                    consoleInput += (char)k;
+                }
+            } else {
+                if(consoleDebug) {
+                    Serial.println("command tool long - cat sat on keyboard? discard data");
+                }
+                consoleInput = "";
+            }
+        }
+        return 0;
+    }
+    return proc_command(k);
+}
+
+int proc_command(int k) {
+    // Single character commands...
+    uint8_t d_samp = 0;
+    switch(k) {
+    case 10:
+    case 13:
+    case 27:
+        return 0;
+    case 'v':
+        version();
+        return 0;
+    case 'h':
+    case '?':
+        return help();
+    case 'd':
+        Serial.println("d = console debug on/off");
+        consoleDebug = !consoleDebug;
+        Serial.print("console debug is now ");
+        Serial.println(consoleDebug);
+        return 0;
+    case 'l':
+        Serial.println("l = toggle line mode");
+        consoleLineMode = !consoleLineMode;
+        Serial.print("line mode is now ");
+        Serial.println(consoleLineMode);
+        return 0;
+    case 'i':
+        d_samp = inputs_read();
+        Serial.print("inputs (BIN):");
+        Serial.println(d_samp, BIN);
+        return 0;
+    default:
+        if(consoleDebug) {
+            Serial.print("proc_command unused: ");
+            Serial.println(k, DEC);
+        }
+    }
+    return 0;
+}
+
+int help() {
+    // this should return all the known commmands
+    Serial.println("h/? = help");
+    Serial.println("v = version");
+    Serial.println("l = toggle line mode");
+    Serial.println("d = console debug on/off");
+    return 0;
+}    
+
+int proc_console_line() {
+    consoleInput.trim();
+    if(consoleInput.length() == 0)
+        return 0;
+    if(consoleDebug) {
+        Serial.print("Line mode: '");
+        Serial.print(consoleInput);
+        Serial.println("'");
+    }
+    if(consoleInput.length() == 1) {
+        return proc_command(consoleInput[0]);
+    }
+    // TODO actually have some line commands!
+    return 0;
+}
